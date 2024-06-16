5cca75b2b545143fe95f598f7a7588174cc6a0f0
Vincent DOLLO <vincent@mobizel.com>
2024-06-16 15:14:57 +0200

    fix error initial commit

--- lib/views/screens/00_auth/login_screen.dart
+++ lib/views/screens/00_auth/login_screen.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_starter/views/components/custom_text_form_field.dart';
 import 'package:flutter_starter/views/components/tap_to_close_keyboard.dart';
-import 'package:flutter_starter/views/viewmodel/00_auth/login_viewmodel.dart';
-import 'package:go_router/go_router.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen(this._loginViewModel, {Key? key}) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
   static const String routeName = 'login';
-
-  final ILoginViewModel _loginViewModel;
 
   @override
   Widget build(BuildContext context) {
@@ -17,54 +13,21 @@ class LoginScreen extends StatelessWidget {
         appBar: AppBar(title: const Text('Login')),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: AnimatedBuilder(
-            animation: _loginViewModel,
-            builder: (context, snapshot) {
-              final isLoading = _loginViewModel.isLoading;
-              return Column(
-                children: [
-                  const SizedBox(height: 20),
-                  CustomTextFormField(
-                    onChanged: _onEmailChange,
-                    errorText: _loginViewModel.emailErrorMessage,
-                  ),
-                  const SizedBox(height: 20),
-                  CustomTextFormField(
-                    onChanged: _onPasswordChange,
-                    errorText: _loginViewModel.passwordErrorMessage,
-                  ),
-                  const SizedBox(height: 50),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await _onClickLogin(context);
-                    },
-                    child: isLoading
-                        ? const CircularProgressIndicator()
-                        : const Text('Login'),
-                  ),
-                ],
-              );
-            },
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              const CustomTextFormField(),
+              const SizedBox(height: 20),
+              const CustomTextFormField(),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Login'),
+              ),
+            ],
           ),
         ),
       ),
     );
-  }
-
-  Future<void> _onClickLogin(BuildContext context) async {
-    await _loginViewModel.loginUser();
-    if (_loginViewModel.user != null) {
-      if (context.mounted) {
-        context.goNamed('screen1');
-      }
-    }
-  }
-
-  void _onEmailChange(String? email) {
-    _loginViewModel.emailChanged(email);
-  }
-
-  void _onPasswordChange(String? password) {
-    _loginViewModel.passwordChanged(password);
   }
 }
--- lib/views/screens/01_home/home_screen.dart
+++ lib/views/screens/01_home/home_screen.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_starter/views/components/tap_to_close_keyboard.dart';
-import 'package:flutter_starter/views/viewmodel/01_home/home_viewmodel.dart';
 
 class Screen1 extends StatelessWidget {
-  const Screen1(this._homeViewmodel, {Key? key}) : super(key: key);
+  const Screen1({Key? key}) : super(key: key);
   static const String routeName = 'screen1';
-
-  final IHomeViewmodel _homeViewmodel;
 
   @override
   Widget build(BuildContext context) {
